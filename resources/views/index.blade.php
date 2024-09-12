d2ab41ce0fd1b46327657daf397139d47fafd07f
mastep <igorchurikov@ya.ru>
2024-09-12 23:47:59 +0300

    include head

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Page Title -->
-    <title>Setupweb | Креативное агентство по созданию сайтов</title>
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&amp;display=swap" rel="stylesheet">
-    <!-- Config -->
-    <link type="text/css" rel="stylesheet" href="css/config.css">
-    <!-- Libraries -->
-    <link type="text/css" rel="stylesheet" href="css/libs.css">
-    <!-- Template Styles -->
-    <link type="text/css" rel="stylesheet" href="css/style.css">
-    <!-- Responsive -->
-    <link type="text/css" rel="stylesheet" href="css/responsive.css">
-
-    <!-- Favicon -->
-    <link rel="icon" href="img/favicon.png" sizes="32x32">
-</head>
+@include('head')
 <body>
 <!-- Header -->
 <header id="bringer-header" class="is-frosted is-sticky" data-appear="fade-down" data-unload="fade-up">
