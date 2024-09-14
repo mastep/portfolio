a1091fcbb2c3c84d31a392f8783f69eb5e564b22
mastep <igorchurikov@ya.ru>
2024-09-14 20:26:12 +0300

    Update title

--- resources/views/protect.blade.php
+++ resources/views/protect.blade.php
@@ -2,6 +2,6 @@
 <div class="bringer-rcp-wrap">
     <div class="bringer-rcp-overlay"></div>
     <div class="bringer-rcp-container">
-        <h2>The context menu is not allowed on this page.</h2>
+        <h2>Меню отключено.</h2>
     </div>
 </div>
