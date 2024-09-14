e881e20969039212caefebcd64f8a5b556806f4c
mastep <igorchurikov@ya.ru>
2024-09-14 15:04:21 +0300

    Revert "Revert "Explode main page""
    This reverts commit b736ae9da701d408726d994abda22f15b7df5f86.

---
+++ resources/views/backlight.blade.php
@@ -0,0 +1,2 @@
+<!-- Dynamic Backlight -->
+<div class="bringer-backlight"></div>
---
+++ resources/views/header-button.blade.php
@@ -0,0 +1,4 @@
+<!-- Header Button -->
+<div class="bringer-header-rp">
+    <a href="contacts.html" class="bringer-button" title="Это бесплатно">Узнать стоимость проекта</a>
+</div>
