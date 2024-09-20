6b1d8ac7d72432b173af727e0e6aae29c272c6aa
mastep <igorchurikov@ya.ru>
2024-09-20 16:21:21 +0300

    update a poster for poster video

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,7 +42,7 @@
                     <!-- Masked Media -->
                     <div class="bringer-masked-media bringer-masked-media bringer-parallax-media">
                         <h2 class="bringer-page-title" data-split-appear="fade-up" data-split-unload="fade-up" style="position:absolute; z-index: 1; margin:10% 0; padding:5%; background: #0b2e13; opacity: 0.5">Покоряйте новые горизонты<br/>Мы предоставим технологии<br>и наш опыт</h2>
-                        <video class="data-poster" autoplay loop muted poster="/img/home/home01-hero.png">
+                        <video class="data-poster" autoplay loop muted poster="/img/home/home01-hero.jpg">
                             <source src="./video/intro.mp4" type="video/mp4">
                         </video>
                     </div>
