bec1f3f53ceea9b5b98d9f8e3fa407c2efafd2aa
mastep <igorchurikov@ya.ru>
2024-09-20 09:46:52 +0300

    update mobile logo mini

--- resources/views/mobileheader.blade.php
+++ resources/views/mobileheader.blade.php
@@ -2,7 +2,7 @@
 <div class="bringer-mobile-header-inner">
     <a href="./" class="bringer-logo" title="Burton.Тим">
         <img class="logo-mobile" src="img/logo.png" width="200">
-        <span class="logo-mobile-xs"><img  src="img/logo-mobile.png"><div class="align-center"><small>Домой</small></div></span>
+        <img class="logo-mobile-xs" src="img/logo-mobile.png">
     </a>
     <a href="/form" class="bringer-button  link-form" title="Это бесплатно">Рассчитать Ваш проект</a>
     <h8 class="ptr_link"></h8>
