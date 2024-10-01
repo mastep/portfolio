3708250b23e01e0f485e16f967a274c9295eb60d
mastep <igorchurikov@ya.ru>
2024-10-01 19:49:32 +0300

    added link-content

---
+++ resources/views/link-content.blade.php
@@ -0,0 +1,15 @@
+<div class="stg-row stg-valign-middle stg-top-gap" data-unload="fade-left">
+    <a href="/form" class="bringer-icon-link">
+        <div class="bringer-icon-wrap">
+            <i class="bringer-icon bringer-icon-explore"></i>
+        </div>
+        <div class="bringer-icon-link-content">
+            <h6>
+                Хотите рассчитаем
+                <br>
+                Ваш проект?
+            </h6>
+            <span class="bringer-label">это бесплатно</span>
+        </div>
+    </a><!-- .bringer-icon-link -->
+</div>
