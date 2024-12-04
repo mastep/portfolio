8b723afa1e931e09e68e59bacf4f1766be2edd9b
mastep <igorchurikov@ya.ru>
2024-12-04 21:02:58 +0300

    updated main_video

--- public/js/main.js
+++ public/js/main.js
@@ -88,15 +88,18 @@ jQuery(function ($) {
             +"<div class=\"bringer-toggles-item--title stg-col-5 stg-m-col-6\"><span class=\"bringer-label\">Telegram<span class=\"bringer-accent\">.</span></span><h9 class='stg-m-bottom-gap'>"+ptb+"</h9></div>"
         );
 
-        $("#main_video").ready(function(){
-            setTimeout(
-                function () {
-                    $(".afterloadhide").hide();
-                    $("#main_video")[0].play();
-                }, 5000
+        if($("#main_video")){
+            $("#main_video").ready(function(){
+                    setTimeout(
+                        function () {
+                            $(".afterloadhide").hide();
+                            $("#main_video")[0].play();
+                        }, 5000
+                    );
+                }
             );
         }
-        );
+
 
         $('video[data-video-src]').each( function(index, element) {
                 $(this).html('<source src="'+$(this).attr('data-video-src')+'" type="video/mp4">');
