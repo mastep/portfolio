3af8c83f46bd36cbf30d3584c3b98b72ae696d45
mastep <igorchurikov@ya.ru>
2024-12-30 02:52:08 +0300

    new intro video 3 (deleted text)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,8 +41,7 @@
                 <div class="bringer-hero-media-wrap bringer-masked-bottom-right bringer-masked-block stg-bottom-gap-l" data-appear="zoom-out" data-unload="zoom-out">
                     <!-- Masked Media -->
                     <div class="bringer-masked-media bringer-masked-media bringer-parallax-media">
-                        <h3 class="bringer-page-title afterloadhide">Покоряйте новые горизонты<br/>Мы предоставим технологии<br>и наш опыт</h3>
-                        <video src="./video/intro.mp4" id="main_video" class="data-poster" poster="/img/home/home01-hero.jpg" loop muted></video>
+                        <video src="./video/intro.mp4" id="main_video" class="data-poster" poster="/img/home/home01-hero.jpg" loop muted autoplay></video>
                     </div>
                     <!-- Content -->
                     <div class="bringer-masked-content at-bottom-right">
