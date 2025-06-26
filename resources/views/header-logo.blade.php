eed01ab88fefe4b3cbc3fc2070554333bc7a5be6
mastep <igorchurikov@ya.ru>
2025-06-26 10:50:29 +0300

    added alt title for logo

--- resources/views/header-logo.blade.php
+++ resources/views/header-logo.blade.php
@@ -1,6 +1,6 @@
 <!-- Header Logo -->
 <div class="bringer-header-lp">
     <a href="./" class="bringer-logo">
-        <img src="img/logo.png" alt="bringer." width="300">
+        <img alt="Разработка сайтов. MVP за 5 дней" title="Разработка сайтов. MVP за 5 дней" src="img/logo.png" alt="bringer." width="300">
     </a>
 </div>
