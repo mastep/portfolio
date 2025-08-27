4a995092d55f81a8660a2d65f4daca580cb6421b
mastep <igorchurikov@ya.ru>
2025-08-27 23:10:13 +0300

    favicon

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -22,7 +22,11 @@
     <link type="text/css" rel="stylesheet" href="css/responsive.css">
 
     <!-- Favicon -->
-    <link rel="icon" href="img/favicon.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
