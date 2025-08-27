91cfa73c0d88fa1466456c3fd86956f1bae02cf1
mastep <igorchurikov@ya.ru>
2025-08-27 11:19:44 +0300

    width

--- resources/views/header-logo.blade.php
+++ resources/views/header-logo.blade.php
@@ -2,9 +2,9 @@
 <div class="bringer-header-lp">
     <a href="./" class="bringer-logo">
         @if(App\Helpers\Helper::isDefProject())
-            <img alt="Разработка сайтов. MVP за 5 дней" title="Разработка сайтов. MVP за 5 дней" src="img/logo-7lab.png" alt="bringer." width="300">
+            <img alt="Разработка сайтов. MVP за 5 дней" title="Разработка сайтов. MVP за 5 дней" src="img/logo-7lab.png" alt="7Lab">
         @else
-            <img alt="Разработка сайтов. MVP за 5 дней" title="Разработка сайтов. MVP за 5 дней" src="img/logo.png" alt="bringer." width="300">
+            <img alt="Разработка сайтов. MVP за 5 дней" title="Разработка сайтов. MVP за 5 дней" src="img/logo.png" alt="Burton" width="300">
         @endif
     </a>
 </div>
