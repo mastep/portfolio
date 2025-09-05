4be7a34cc67268384896b9c1bdb9991ca5656ae6
mastep <igorchurikov@ya.ru>
2025-09-05 18:41:08 +0300

    SEO - BreadcrumbList

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,42 +1,55 @@
 <!-- Main Menu -->
 <div class="bringer-header-mp">
     <nav class="bringer-nav">
-        <ul class="main-menu" data-stagger-appear="fade-down" data-stagger-delay="75">
-            <li>
-                <a href="/">Главная</a>
+        <ul class="main-menu" data-stagger-appear="fade-down" data-stagger-delay="75" itemscope itemtype="https://schema.org/BreadcrumbList">
+            <li itemprop="itemListElement" itemscope
+                itemtype="https://schema.org/ListItem">
+                <a itemprop="item" href="/">
+                    <span itemprop="name">Главная</span></a>
+                    <meta itemprop="position" content="1" />
+                </a>
             </li>
-            <li>
-                <a href="#">Услуги</a>
+            <li itemprop="itemListElement" itemscope
+                itemtype="https://schema.org/ListItem">
+                <a itemprop="item" href="#">
+                    <span itemprop="name">Услуги</span></a>
+                    <meta itemprop="position" content="2" />
+                    </a>
                 <ul class="sub-menu">
                     <li>
-                        <a href="/e-commerce">Интернет-магазины</a>
+                        <a itemprop="item" href="/e-commerce">Интернет-магазины</a>
                     </li>
                     <li>
-                        <a href="/corporate-portal">Корпоративные порталы</a>
+                        <a itemprop="item" href="/corporate-portal">Корпоративные порталы</a>
                     </li>
                     <li>
-                        <a href="/landing-page">Сайты-визитки</a>
+                        <a itemprop="item" href="/landing-page">Сайты-визитки</a>
                     </li>
                     <li>
-                        <a href="/it-sharing">IT - шеринг</a>
+                        <a itemprop="item" href="/it-sharing">IT - шеринг</a>
                     </li>
                 </ul>
             </li>
-            <li>
-                <a href="/#ourPortfolio" onclick="document.getElementsByClassName('bringer-mobile-menu-toggler')[0].click();">Портфолио</a>
+            <li itemprop="itemListElement" itemscope
+                itemtype="https://schema.org/ListItem">
+                <a itemprop="item" href="/#ourPortfolio" onclick="document.getElementsByClassName('bringer-mobile-menu-toggler')[0].click();">
+                    <span itemprop="name">Портфолио</span></a>
+                    <meta itemprop="position" content="3" />
+                </a>
             </li>
 
-            <li>
+            <li itemprop="itemListElement" itemscope
+                itemtype="https://schema.org/ListItem">
                 <a href="#">Контакты</a>
                 <ul class="sub-menu">
                     <li>
-                        <a class="phb_link"></a>
+                        <a itemprop="item" class="phb_link"></a>
                     </li>
                     <li>
-                        <a class="pmb_link"></a>
+                        <a itemprop="item" class="pmb_link"></a>
                     </li>
                     <li>
-                        <a class="ptb_link"></a>
+                        <a itemprop="item" class="ptb_link"></a>
                     </li>
                 </ul>
             </li>
