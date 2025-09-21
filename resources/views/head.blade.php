6b2a68ad4ec851939b2c148125146b9d4beaf5a0
mastep <igorchurikov@ya.ru>
2025-09-22 00:50:18 +0300

    favicon https

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -24,7 +24,7 @@
     <!-- Favicon -->
     <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-    <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="https://7lab.pro/favicon/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="/favicon/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="7Lab.pro" />
