5932fc8d95f9b252f0d1be0bea5882c3236262fe
mastep <hello@7lab.com>
2025-12-07 14:51:12 +0300

    routes have been moved to the api

--- routes/web.php
+++ routes/web.php
@@ -53,17 +53,7 @@ Route::get('telegram-bot', function () {
     return view('service-telegram-bot');
 });
 
-// Telegram Bot routes
-Route::post('/telegram/webhook', [App\Http\Controllers\TelegramController::class, 'webhook']);
-Route::get('/webapp', [App\Http\Controllers\WebAppController::class, 'index']);
-Route::get('/api/products', [App\Http\Controllers\WebAppController::class, 'api']);
 
-
-/*Proxy для обхода CSRF - удалить d PROD*/
-Route::get('/telegram/webhook_test',function (Illuminate\Http\Request $request){
-    $update = $request->all();
-    echo $answer = QA::findAnswer($update['message']['text'])?? 'Извините, я не нашел ответ на ваш вопрос. Попробуйте переформулировать вопрос или обратитесь к администратору.';
-});
 
 
 
