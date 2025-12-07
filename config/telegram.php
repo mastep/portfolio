7e1d3778b58be9987713ab867a4c0179c18a945b
mastep <hello@7lab.com>
2025-12-07 14:53:04 +0300

    correted telegram token

--- config/telegram.php
+++ config/telegram.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'bot_token' => env('TELEGRAM_BOT_TOKEN'),
+    'bot_token' => env('TELEGRAM_TOKEN'),
     'web_app_url' => env('TELEGRAM_WEB_APP_URL', 'http://localhost/webapp'),
     'api_url' => 'https://api.telegram.org/bot',
 ];
