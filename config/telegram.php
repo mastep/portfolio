59b5f2fa76069d54f37b65ca7099a3e983fff864
mastep <hello@7lab.com>
2025-12-08 21:25:27 +0300

    corrected url

--- config/telegram.php
+++ config/telegram.php
@@ -2,7 +2,7 @@
 
 return [
     'bot_token' => env('TELEGRAM_TOKEN'),
-    'web_app_url' => env('TELEGRAM_WEB_APP_URL', 'http://localhost/webapp'),
+    'web_app_url' => env('TELEGRAM_WEB_APP_URL', 'http://localhost/api/webapp'),
     'api_url' => 'https://api.telegram.org/bot',
 ];
 
