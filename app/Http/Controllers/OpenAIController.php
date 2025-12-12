38b103fc66b0dab8c0295649124a2cf68cfe63a1
mastep <hello@7lab.com>
2025-12-12 13:44:47 +0300

    AI debug - ON (production)

--- app/Http/Controllers/OpenAIController.php
+++ app/Http/Controllers/OpenAIController.php
@@ -4,32 +4,37 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class OpenAIController extends Controller
 {
     public function generateText($prompt="", $data=false)
     {
-        if(is_array($data)&&!empty($data)){
-            $prompt='Представь, что ты чат бот. Ответь на вопрос пользователя исходя из текстовой базы знаний. Если не удается найти ответ в базе знаний ничего не отвечай.
-Вопрос:'.$prompt.'
-База знаний:'.print_r($data,true);
+        try {
+            if(is_array($data)&&!empty($data)){
+                $prompt='Представь, что ты сотрудник технической поддержки. Отвечай очень вежливо. Помоги ответить на вопрос пользователя исходя из текстовой базы знаний. Если не нашел ответ в базе знаний ничего не отвечай message оставь пустым. Не сообщай что ты ищешь ответ в нашей базе знаний. Не используй символы **. Вопрос:'.$prompt.'. База знаний:'.print_r($data, true);
+            }
+            $client = new Client();
+            $response = $client->post(config('open_ai.url'), [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . config('open_ai.token'),
+                    'x-proxy-source' => '',
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => [
+                    'message' => $prompt,
+                    'parent_message_id' => '',
+                    'file_ids' => ['']
+                ],
+            ]);
+
+            $body = json_decode($response->getBody(), true);
+        } catch (Exception $e) {
+            $errorMessage = "Ошибка запроса к AI: " . $e->getMessage();
+            $body['message']=$errorMessage; //todo убрать после отладки
+            Log::error($errorMessage);
         }
-        $client = new Client();
-        $response = $client->post(config('open_ai.url'), [
-            'headers' => [
-                'Authorization' => 'Bearer ' . config('open_ai.token'),
-                'x-proxy-source' => '',
-                'Content-Type' => 'application/json',
-            ],
-            'json' => [
-                'message' => $prompt,
-                'parent_message_id' => '',
-                'file_ids' => ['']
-            ],
-        ]);
 
-
-        $body = json_decode($response->getBody(), true);
 
         return $body;
     }
