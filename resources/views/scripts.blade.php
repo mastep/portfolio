e080b20d5b45d51e31def4d9d0a3358058b89fee
mastep <hello@7lab.com>
2025-12-15 14:34:12 +0300

    AI CHAT BOT

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -5,3 +5,4 @@
 <script src="js/st-core.js"></script>
 <script src="js/classes.js"></script>
 <script src="js/main.js"></script>
+<script async src="https://timeweb.cloud/api/v1/cloud-ai/agents/f81d5fec-61c3-44c5-90f3-360e2000ea3a/embed.js?collapsed=true"></script>
