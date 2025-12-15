7996d83f5ab3f9f5f024798d3b729585c112c53c
mastep <hello@7lab.com>
2025-12-15 14:40:34 +0300

    new title logo

--- resources/views/header-logo.blade.php
+++ resources/views/header-logo.blade.php
@@ -2,9 +2,9 @@
 <div class="bringer-header-lp">
     <a href="./" class="bringer-logo">
         @if(App\Helpers\Helper::isDefProject())
-            <img alt="Разработка сайтов. MVP за 5 дней" title="Разработка сайтов. MVP за 5 дней" src="img/logo-7lab.svg" alt="7Lab" width="150">
+            <img alt="It решения для бизнеса. MVP за 5 дней. Чат боты, api, ecommerce" title="It решения для бизнеса. MVP за 5 дней. Чат боты, api, ecommerce" src="img/logo-7lab.svg" alt="7Lab" width="150">
         @else
-            <img alt="Разработка сайтов. MVP за 5 дней" title="Разработка сайтов. MVP за 5 дней" src="img/logo.png" alt="Burton" width="300">
+            <img alt="It решения для бизнеса. MVP за 5 дней. Чат боты, api, ecommerce" title="It решения для бизнеса. MVP за 5 дней. Чат боты, api, ecommerce" src="img/logo.png" alt="Burton" width="300">
         @endif
     </a>
 </div>
