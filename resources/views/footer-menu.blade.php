9125ca97ee5af98a49a927b75c581848ddcae8d0
mastep <hello@7lab.com>
2026-02-09 11:42:33 +0300

    added OpenClaw

--- resources/views/footer-menu.blade.php
+++ resources/views/footer-menu.blade.php
@@ -3,7 +3,7 @@
         <h6>Наш cтек</h6>
         <div class="bringer-menu-widget">
             <ul>
-                <li><a href="#">Cursor, Nginx, PHP</a></li>
+                <li><a href="#">OpenClaw, Cursor, Nginx, PHP</a></li>
                 <li><a href="#">Javascript, Vue</a></li>
                 <li><a href="#">Kubernetes, Docker, GitHub</a></li>
                 <li><a href="#">Memcached, Redis, Elasticsearch</a></li>
