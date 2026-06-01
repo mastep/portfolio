171020bcf79f7d022097103b74a8cf9d5d283353
mastep <hello@7lab.com>
2026-06-02 00:43:29 +0300

    Version Lab

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -66,7 +66,7 @@
         color: #fff;
         text-align: center;
         font-size: 1em;
-        content: 'Лаборатория №7: Инженерия цифровых решений';
+        content: 'Лаборатория AI инженерии для бизнеса';
         position: absolute;
         top: 50%;
         left: 50%;
